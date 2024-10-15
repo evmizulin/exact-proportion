6a2721a7301dd663de20c2af34e7da77c1b81d00
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2024-10-15 07:51:38 +0300

    fix

--- build/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const exactProportion: {
-    a: (a: number) => {
-        ratioB: (b: number) => {
-            likeC: (c: number) => {
-                ratioX: () => number;
-            };
-        };
-    };
-};
